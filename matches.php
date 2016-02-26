3072da4a54245faa3cd5476dc0403fcd2e68ab22
Shobi P P <ppshobi@gmail.com>
2016-02-26 20:42:12 +0530

    Sperated happy marrige life side bar in search page and view profile pahe

--- matches.php
+++ matches.php
@@ -327,70 +327,9 @@ $(document).ready(function(){
  	  <li><a href="#">4</a></li>
 	</ul>
   </div>
-  <div class="col-md-3 match_right">
-    <ul class="menu">
-		<li class="item1"><h3 class="m_2">Show Profiles Created</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">within a week (2) </a></li>
-			<li class="subitem2"><a href="#">within a month (4)</a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Profile type</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">with Photo (3) </a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Marital Status</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Unmarried (2) </a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Mother Tongue</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">English </a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Education</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Bachelors-Engineering </a></li>
-			<li class="subitem1"><a href="#">Masters-Engineering </a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Occupation</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Engineer-Non IT (2) </a></li>
-			<li class="subitem1"><a href="#">Software Professional (3)</a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Physical Status</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Normal (2) </a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Eating Habits</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Non Vegetarian (3)</a></li>
-			<li class="subitem1"><a href="#">Vegetarian (2)</a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Smoking</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Not Specified (3)</a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Drinking</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Not Specified (3)</a></li>
-			<li class="subitem1"><a href="#">Never Drinks (3)</a></li>
-		  </ul>
-		</li>
-		<li class="item1"><h3 class="m_2">Profile Created by</h3>
-		  <ul class="cute">
-			<li class="subitem1"><a href="#">Self (1)</a></li>
-		  </ul>
-		</li>
-	  </ul>
-   </div>
+  <!-- Match Right -->
+  <?php include_once("includes/matchright.php");?>
+  <!-- Match Rioght end -->
    <div class="clearfix"> </div>
   </div>
 </div>
