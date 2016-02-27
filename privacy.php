2ffc544ff82c690ac28e9ef638a781d385de75de
Shobi P P <ppshobi@gmail.com>
2016-02-27 15:16:34 +0530

    seperated footer file

--- privacy.php
+++ privacy.php
@@ -82,43 +82,7 @@ $(document).ready(function(){
 </div>
 
 
-<div class="footer">
-    	<div class="container">
-    		<div class="col-md-4 col_2">
-    			<h4>About Us</h4>
-    			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."</p>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Help & Support</h4>
-    			<ul class="footer_links">
-    				<li><a href="#">24x7 Live help</a></li>
-    				<li><a href="contact.php">Contact us</a></li>
-    				<li><a href="#">Feedback</a></li>
-    				<li><a href="faq.php">FAQs</a></li>
-    			</ul>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Quick Links</h4>
-    			<ul class="footer_links">
-    				<li><a href="privacy.php">Privacy Policy</a></li>
-    				<li><a href="terms.php">Terms and Conditions</a></li>
-    				<li><a href="services.php">Services</a></li>
-    			</ul>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Social</h4>
-    			<ul class="footer_social">
-				  <li><a href="#"><i class="fa fa-facebook fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-google-plus fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-youtube fa1"> </i></a></li>
-			    </ul>
-    		</div>
-    		<div class="clearfix"> </div>
-    		<div class="copy">
-		       <p>Copyright © 2015 Marital . All Rights Reserved  | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-	        </div>
-      </div>
-</div>
+<?php include_once("footer.php");?>
+
 </body>
 </html>	
--- register.php
+++ register.php
@@ -71,37 +71,37 @@ $(document).ready(function(){
                   <div class="select-block1">
                     <select name="day">
 	                    <option value="">Date</option>
-	                    <option value="">1</option>
-	                    <option value="">2</option>
-	                    <option value="">3</option>
-	                    <option value="">4</option>
-	                    <option value="">5</option>
-	                    <option value="">6</option>
-	                    <option value="">7</option>
-	                    <option value="">8</option>
-	                    <option value="">9</option>
-	                    <option value="">10</option>
-	                    <option value="">11</option>
-	                    <option value="">12</option>
-	                    <option value="">13</option>
-	                    <option value="">14</option>
-	                    <option value="">15</option>
-	                    <option value="">16</option>
-	                    <option value="">17</option>
-	                    <option value="">18</option>
-	                    <option value="">19</option>
-	                    <option value="">20</option>
-	                    <option value="">21</option>
-	                    <option value="">22</option>
-	                    <option value="">23</option>
-	                    <option value="">24</option>
-	                    <option value="">25</option>
-	                    <option value="">26</option>
-	                    <option value="">27</option>
-	                    <option value="">28</option>
-	                    <option value="">29</option>
-	                    <option value="">30</option>
-	                    <option value="">31</option>
+	                     <option value="1">1</option>
+		                    <option value="2">2</option>
+		                    <option value="3">3</option>
+		                    <option value="4">4</option>
+		                    <option value="5">5</option>
+		                    <option value="6">6</option>
+		                    <option value="7">7</option>
+		                    <option value="8">8</option>
+		                    <option value="9">9</option>
+		                    <option value="10">10</option>
+		                    <option value="11">11</option>
+		                    <option value="12">12</option>
+		                    <option value="13">13</option>
+		                    <option value="14">14</option>
+		                    <option value="15">15</option>
+		                    <option value="16">16</option>
+		                    <option value="17">17</option>
+		                    <option value="18">18</option>
+		                    <option value="19">19</option>
+		                    <option value="20">20</option>
+		                    <option value="21">21</option>
+		                    <option value="22">22</option>
+		                    <option value="23">23</option>
+		                    <option value="24">24</option>
+		                    <option value="25">25</option>
+		                    <option value="26">26</option>
+		                    <option value="27">27</option>
+		                    <option value="28">28</option>
+		                    <option value="29">29</option>
+		                    <option value="30">30</option>
+		                    <option value="31">31</option>
                     </select>
                   </div>
             </div>
@@ -109,18 +109,18 @@ $(document).ready(function(){
                    <div class="select-block1">
                     <select name="month">
 	                    <option value="">Month</option>
-	                    <option value="">January</option>
-	                    <option value="">February</option>
-	                    <option value="">March</option>
-	                    <option value="">April</option>
-	                    <option value="">May</option>
-	                    <option value="">June</option>
-	                    <option value="">July</option>
-	                    <option value="">August</option>
-	                    <option value="">September</option>
-	                    <option value="">October</option>
-	                    <option value="">November</option>
-	                    <option value="">December</option>
+	                    <option value="01">January</option>
+	                    <option value="02">February</option>
+	                    <option value="03">March</option>
+	                    <option value="04">April</option>
+	                    <option value="05">May</option>
+	                    <option value="06">June</option>
+	                    <option value="07">July</option>
+	                    <option value="08">August</option>
+	                    <option value="09">September</option>
+	                    <option value="10">October</option>
+	                    <option value="11">November</option>
+	                    <option value="12">December</option>
                     </select>
                   </div>
                  </div>
@@ -128,42 +128,33 @@ $(document).ready(function(){
                    <div class="select-block1">
                     <select name="year">
 	                    <option value="">Year</option>
-	                    <option value="">1980</option>
-	                    <option value="">1981</option>
-	                    <option value="">1982</option>
-	                    <option value="">1983</option>
-	                    <option value="">1984</option>
-	                    <option value="">1985</option>
-	                    <option value="">1986</option>
-	                    <option value="">1987</option>
-	                    <option value="">1988</option>
-	                    <option value="">1989</option>
-	                    <option value="">1990</option>
-	                    <option value="">1991</option>
-	                    <option value="">1992</option>
-	                    <option value="">1993</option>
-	                    <option value="">1994</option>
-	                    <option value="">1995</option>
-	                    <option value="">1996</option>
-	                    <option value="">1997</option>
-	                    <option value="">1998</option>
-	                    <option value="">1999</option>
-	                    <option value="">2000</option>
-	                    <option value="">2001</option>
-	                    <option value="">2002</option>
-	                    <option value="">2003</option>
-	                    <option value="">2004</option>
-	                    <option value="">2005</option>
-	                    <option value="">2006</option>
-	                    <option value="">2007</option>
-	                    <option value="">2008</option>
-	                    <option value="">2009</option>
-	                    <option value="">2010</option>
-	                    <option value="">2011</option>
-	                    <option value="">2012</option>
-	                    <option value="">2013</option>
-	                    <option value="">2014</option>
-	                    <option value="">2015</option>
+		                    <option value="1980">1980</option>
+		                    <option value="1981">1981</option>
+		                    <option value="1981">1981</option>
+		                    <option value="1983">1983</option>
+		                    <option value="1984">1984</option>
+		                    <option value="1985">1985</option>
+		                    <option value="1986">1986</option>
+		                    <option value="1987">1987</option>
+		                    <option value="1988">1988</option>
+		                    <option value="1989">1989</option>
+		                    <option value="1990">1990</option>
+		                    <option value="1991">1991</option>
+		                    <option value="1992">1992</option>
+		                    <option value="1993">1993</option>
+		                    <option value="1994">1994</option>
+		                    <option value="1995">1995</option>
+		                    <option value="1996">1996</option>
+		                    <option value="1997">1997</option>
+		                    <option value="1998">1998</option>
+		                    <option value="1999">1999</option>
+		                    <option value="2000">2000</option>
+		                    <option value="2001">2001</option>
+		                    <option value="2002">2002</option>
+		                    <option value="2003">2003</option>
+		                    <option value="2004">2004</option>
+		                    <option value="2005">2005</option>
+		                    <option value="2006">2006</option>
                     </select>
                    </div>
                   </div>
@@ -205,43 +196,7 @@ $(document).ready(function(){
 </div>
 
 
-<div class="footer">
-    	<div class="container">
-    		<div class="col-md-4 col_2">
-    			<h4>About Us</h4>
-    			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."</p>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Help & Support</h4>
-    			<ul class="footer_links">
-    				<li><a href="#">24x7 Live help</a></li>
-    				<li><a href="contact.php">Contact us</a></li>
-    				<li><a href="#">Feedback</a></li>
-    				<li><a href="faq.php">FAQs</a></li>
-    			</ul>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Quick Links</h4>
-    			<ul class="footer_links">
-    				<li><a href="privacy.php">Privacy Policy</a></li>
-    				<li><a href="terms.php">Terms and Conditions</a></li>
-    				<li><a href="services.php">Services</a></li>
-    			</ul>
-    		</div>
-    		<div class="col-md-2 col_2">
-    			<h4>Social</h4>
-    			<ul class="footer_social">
-				  <li><a href="#"><i class="fa fa-facebook fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-google-plus fa1"> </i></a></li>
-				  <li><a href="#"><i class="fa fa-youtube fa1"> </i></a></li>
-			    </ul>
-    		</div>
-    		<div class="clearfix"> </div>
-    		<div class="copy">
-		       <p>Copyright © 2015 Marital . All Rights Reserved  | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-	        </div>
-      </div>
-</div>
+<?php include_once("footer.php");?>
+
 </body>
 </html>	
